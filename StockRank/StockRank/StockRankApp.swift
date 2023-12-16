d35ec462ba863dc8c3e1f8cb2dbd16cdc610b0f5
Woobios97 <138302237+Woobios97@users.noreply.github.com>
2023-12-17 02:46:29 +0900

    feature/SwiftUI-ListAndGrid-StockRank

---
+++ StockRank/StockRank/StockRankApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockRankApp.swift
+//  StockRank
+//
+//  Created by 김우섭 on 12/17/23.
+//
+
+import SwiftUI
+
+@main
+struct StockRank_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            StockRankView()
+        }
+    }
+}
---
+++ StockRank/StockRank/StockRankView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  StockRank
+//
+//  Created by 김우섭 on 12/17/23.
+//
+
+import SwiftUI
+
+struct StockRankView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        StockRankView()
+    }
+}
+
