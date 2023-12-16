363fe5b1d53d0b5e75131c69dec4648356676785
김우섭 <woobios97@gmail.com>
2023-12-16 16:04:12 +0900

    Add files via upload

---
+++ SwiftUI-Basic/SwiftUI-Basic/SwiftUI_BasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BasicApp.swift
+//  SwiftUI-Basic
+//
+//  Created by 김우섭 on 12/16/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
